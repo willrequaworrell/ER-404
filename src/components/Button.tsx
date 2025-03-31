88901b74f258bfc3e4be9073dc588593f824c920
Will Worrell <willrequaworrell@gmail.com>
2025-03-31 17:24:52 -0400

    update play button to be disabled when playing, appear activated/pressed, and return to unactivated style when play stops

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,19 @@
 import { MouseEventHandler, ReactNode } from "react"
+import { useTracksContext } from "../context/TracksContext"
 
 interface ButtonPropsType {
     icon: ReactNode
+    playButton?: boolean
     styles?: string
     onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-const Button = ({icon, styles="", onClick}:ButtonPropsType) => {
+const activatedStyles = "shadow-[inset_-4px_-4px_9px_#ffffffe0,inset_2px_2px_4px_#718eab1a]"
+const activatedStylesPlayButton = "shadow-[inset_-4px_-4px_9px_#ffffffe0,inset_2px_2px_4px_#718eab1a] cursor-not-allowed"
+
+const Button = ({icon, playButton=false, styles="", onClick}:ButtonPropsType) => {
+    const {isPlaying} = useTracksContext()
+
     return (
         <button 
             type="button"
@@ -18,11 +25,13 @@ const Button = ({icon, styles="", onClick}:ButtonPropsType) => {
                 bg-background 
                 rounded-2xl 
                 shadow-[4px_4px_6px_#b0c0c9,-4px_-4px_6px_#ffffff]
-                active:shadow-[inset_-4px_-4px_9px_#ffffffe0,inset_2px_2px_4px_#718eab1a]
                 transition-all
                 cursor-pointer
+                active:${!playButton && activatedStyles}
                 ${styles} 
+                ${(playButton && isPlaying) && activatedStylesPlayButton}
             `}
+            disabled={playButton && isPlaying}
         >
             {icon}
         </button>
