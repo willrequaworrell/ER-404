a4041d4e2fe737728275b492245a9f57f3c6b9cf
Will Worrell <willrequaworrell@gmail.com>
2025-03-31 17:32:43 -0400

    duplicated speaker and made each a bit wider

--- src/components/Speaker.tsx
+++ src/components/Speaker.tsx
@@ -1,43 +1,49 @@
 const Speaker = () => {
     const rows = [
         // 28, // Second row: 5 holes
-        32, // Middle row: 7 holes
-        35,
-        36,
-        36,
-        36,
-        36,
-        36,
-        35,
-        32,
+        44, // Middle row: 7 holes
+        46,
+        48,
+        48,
+        48,
+        48,
+        48,
+        46,
+        44,
     ];
-    // const rows = [
-    //     4, // Second row: 5 holes
-    //     7, // Middle row: 7 holes
-    //     9,
-    //     10,
-    //     10,
-    //     10,
-    //     9,
-    //     7,
-    //     4, // Fourth row: 5 holes
-    // ];
 
     return (
-        <div className="absolute flex flex-col items-center justify-center gap-1 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            {rows.map((cols, rowIndex) => (
-                <div
-                    key={rowIndex}
-                    className="flex justify-center gap-1"
-                >
-                    {Array.from({ length: cols }).map((_, colIndex) => (
-                        <div
-                            key={colIndex}
-                            className="w-1 h-1 bg-black rounded-full"
-                        ></div>
-                    ))}
-                </div>
-            ))}
+        <div className="absolute flex items-center -translate-x-1/2 -translate-y-1/2 gap-x-24 top-1/2 left-1/2">
+            <div className="flex flex-col items-center justify-center gap-1">
+                {rows.map((cols, rowIndex) => (
+                    <div
+                        key={rowIndex}
+                        className="flex justify-center gap-1"
+                    >
+                        {Array.from({ length: cols }).map((_, colIndex) => (
+                            <div
+                                key={colIndex}
+                                className="w-1 h-1 bg-black rounded-full"
+                            ></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            <div className="flex flex-col items-center justify-center gap-1">
+                {rows.map((cols, rowIndex) => (
+                    <div
+                        key={rowIndex}
+                        className="flex justify-center gap-1"
+                    >
+                        {Array.from({ length: cols }).map((_, colIndex) => (
+                            <div
+                                key={colIndex}
+                                className="w-1 h-1 bg-black rounded-full"
+                            ></div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
