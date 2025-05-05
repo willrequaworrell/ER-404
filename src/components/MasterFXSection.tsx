0ec788d7925194b7d657a953268265edeb83169e
Will Worrell <willrequaworrell@gmail.com>
2025-05-05 00:50:21 -0400

    Update EQ section label and frequency analyzer bar height

--- src/components/MasterFXSection.tsx
+++ src/components/MasterFXSection.tsx
@@ -33,7 +33,7 @@ const MasterFXSection = () => {
                 <div className="absolute w-full  h-[2px] -top-3 bg-text-primary "></div>
                     <div className="absolute h-2 border-l left-0 -top-3 border-1 border-text-primary"></div>
                     <div className="absolute h-2 border-r right-0 -top-3 border-1 border-text-primary"></div>
-                    <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -top-6 left-1/2">EQ</span>
+                    <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -top-6 left-1/2">EQUALIZER</span>
                     <Knob 
                         id="masterEQLow"
                         label="Bass"
