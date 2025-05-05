f6ce4f3ad4803872f3eded8a3f60f03c7f02d4b1
Will Worrell <willrequaworrell@gmail.com>
2025-05-04 23:59:39 -0400

    added section top label to match compressor section

--- src/components/MasterFXSection.tsx
+++ src/components/MasterFXSection.tsx
@@ -29,42 +29,47 @@ const MasterFXSection = () => {
         <div>
             <div className="relative flex items-end h-full pb-3 pr-2 border-b-2 gap-x-[1vw] border-text-primary">
                 <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -bottom-3 left-1/2">MASTER</span>
+                <div className="relative flex gap-x-[1vw] ">
+                <div className="absolute w-full  h-[2px] -top-3 bg-text-primary "></div>
+                    <div className="absolute h-2 border-l left-0 -top-3 border-1 border-text-primary"></div>
+                    <div className="absolute h-2 border-r right-0 -top-3 border-1 border-text-primary"></div>
+                    <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -top-6 left-1/2">EQ</span>
+                    <Knob 
+                        id="masterEQLow"
+                        label="Bass"
+                        value={masterFXSettings.eqLow}
+                        min={0}
+                        max={100}
+                        valueFormatter={formatMasterEQKnob}
+                        onChange={handleKnobChange}
+                        onDoubleClick={() => resetMasterFXKnobValue("masterEQLow")}
+                    />
+                    <Knob 
+                        id="masterEQMid"
+                        label="Mid"
+                        value={masterFXSettings.eqMid}
+                        min={0}
+                        max={100}
+                        valueFormatter={formatMasterEQKnob}
+                        onChange={handleKnobChange}
+                        onDoubleClick={() => resetMasterFXKnobValue("masterEQMid")}
+                    />
+                    <Knob 
+                        id="masterEQHigh"
+                        label="Treble"
+                        value={masterFXSettings.eqHigh}
+                        min={0}
+                        max={100}
+                        valueFormatter={formatMasterEQKnob}
+                        onChange={handleKnobChange}
+                        onDoubleClick={() => resetMasterFXKnobValue("masterEQHigh")}
+                    />
+                </div>
                 
-                <Knob 
-                    id="masterEQLow"
-                    label="Bass"
-                    value={masterFXSettings.eqLow}
-                    min={0}
-                    max={100}
-                    valueFormatter={formatMasterEQKnob}
-                    onChange={handleKnobChange}
-                    onDoubleClick={() => resetMasterFXKnobValue("masterEQLow")}
-                />
-                <Knob 
-                    id="masterEQMid"
-                    label="Mid"
-                    value={masterFXSettings.eqMid}
-                    min={0}
-                    max={100}
-                    valueFormatter={formatMasterEQKnob}
-                    onChange={handleKnobChange}
-                    onDoubleClick={() => resetMasterFXKnobValue("masterEQMid")}
-                />
-                <Knob 
-                    id="masterEQHigh"
-                    label="Treble"
-                    value={masterFXSettings.eqHigh}
-                    min={0}
-                    max={100}
-                    valueFormatter={formatMasterEQKnob}
-                    onChange={handleKnobChange}
-                    onDoubleClick={() => resetMasterFXKnobValue("masterEQHigh")}
-                />
-                
-                <div className="relative flex gap-x-[1vw] justify-between  border-text-primary">
-                    <div className="absolute w-[110%] -translate-x-[5%] h-[2px] -top-3 bg-text-primary "></div>
-                    <div className="absolute h-2 border-l -left-2 -top-3 border-1 border-text-primary"></div>
-                    <div className="absolute h-2 border-r -right-2 -top-3 border-1 border-text-primary"></div>
+                <div className="relative flex gap-x-[1vw] justify-between  border-text-primary ">
+                    <div className="absolute w-full h-[2px] -top-3 bg-text-primary "></div>
+                    <div className="absolute h-2 border-l left-0 -top-3 border-1 border-text-primary"></div>
+                    <div className="absolute h-2 border-r right-0 -top-3 border-1 border-text-primary"></div>
                     <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -top-6 left-1/2">COMPRESSOR</span>
                     <Knob
                         id="masterCompressorRatio"
