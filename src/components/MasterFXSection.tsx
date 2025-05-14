4001201d51e70e9287e4a02f6609ef63d533e15d
Will Worrell <willrequaworrell@gmail.com>
2025-05-14 18:07:16 -0400

    simplify knob mapping on Master fx section

--- src/components/MasterFXSection.tsx
+++ src/components/MasterFXSection.tsx
@@ -1,7 +1,8 @@
 // import { useState } from "react"
 import Knob from "./Knob"
 import { useTracksContext } from "../context/TracksContext"
-import {formatMasterCompressorRatio, formatMasterCompressorThreshold, formatMasterEQKnob, formatMasterVolume } from "../util/knobValueHelpers"
+import {formatMasterCompressorRatio, formatMasterCompressorThreshold, formatMasterEQKnob, formatMasterVolume, mapKnobIdToProperty } from "../util/knobValueHelpers"
+import { MasterFXSettingsType } from "../types/masterFXSettings"
 
 
 const MasterFXSection = () => {
@@ -9,19 +10,7 @@ const MasterFXSection = () => {
 
 
     const handleKnobChange = (knobId: string, newValue: number) => {
-        if (knobId === "masterVolume") {
-            handleSetMasterFXSettings("volume", newValue)
-        } else if (knobId === "masterEQLow") {
-            handleSetMasterFXSettings("eqLow", newValue)
-        } else if (knobId === "masterEQMid") {
-            handleSetMasterFXSettings("eqMid", newValue)
-        } else if (knobId === "masterEQHigh") {
-            handleSetMasterFXSettings("eqHigh", newValue)
-        } else if (knobId === "masterCompressorRatio") {
-            handleSetMasterFXSettings("compressorRatio", newValue)
-        } else if (knobId === "masterCompressorThreshold") {
-            handleSetMasterFXSettings("compressorThreshold", newValue)
-        } 
+        handleSetMasterFXSettings(mapKnobIdToProperty(knobId) as keyof MasterFXSettingsType, newValue)
     }
 
     
@@ -30,7 +19,7 @@ const MasterFXSection = () => {
             <div className="relative flex items-end h-full pb-3 pr-2 border-b-2 gap-x-[1vw] border-text-primary">
                 <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -bottom-3 left-1/2">MASTER</span>
                 <div className="relative flex gap-x-[1vw] ">
-                <div className="absolute w-full  h-[2px] -top-3 bg-text-primary "></div>
+                    <div className="absolute w-full  h-[2px] -top-3 bg-text-primary "></div>
                     <div className="absolute h-2 border-l left-0 -top-3 border-1 border-text-primary"></div>
                     <div className="absolute h-2 border-r right-0 -top-3 border-1 border-text-primary"></div>
                     <span className="absolute px-2 text-[.85rem] -translate-x-1/2 text-text-primary bg-background -top-6 left-1/2">EQUALIZER</span>
